69bbf02773b6bec5eb22615139b107e028b23ce1
Ajay Susanth <ajaysusanth10@gmail.com>
2024-07-30 20:27:22 +0530

    create-account

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,14 @@ const {authenticate} = require('./utilities')
 
 const app = express()
 
+app.use(express.json())
+
+app.use(
+    cors({
+        origin:"*"
+    })
+)
+
 mongoose.connect(config.connectionString)
 .then(()=>{
     console.log('Connected to db')
@@ -19,11 +27,51 @@ mongoose.connect(config.connectionString)
     console.log(err)
 })
 
-app.use(
-    cors({
-        origin:"*"
+const User = require('./models/user.model')
+
+
+app.get('/',(req,res)=>{
+    res.json({data:'hello'})
+})
+
+// Create account
+
+app.post('/create-account',async (req,res)=>{
+    const {fullName,email,password} = req.body
+    
+    if(!fullName) {
+        return res.status(400).json({error:true,message:"Full Name is required"})
+    }
+
+    if(!email) {
+        return res.status(400).json({error:true,message:"Email is required"})
+    }
+
+    if(!password) {
+        return res.status(400).json({error:true,message:"Password is required"})
+    }
+
+    const isUser = await User.findOne({email:email})
+
+    if(isUser){
+        return res.json({error:true,message:"User already exist. Please login"})
+    }
+
+    const user = new User({
+        fullName,
+        email,
+        password
     })
-)
+
+    await user.save() // Saves the data to a document in mongodb
+
+    const accessToken = jwt.sign({user},process.env.ACCESS_TOKEN,{
+        expiresIn:"36000m"
+    })
+    return res.json({error:false,user,accessToken,message:"Registered Successfully"})
+})
+
+
 
 app.listen(8000)
 
