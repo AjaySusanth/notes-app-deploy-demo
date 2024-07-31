b232cb1270d6bb236a6d1ff49af0198b5b10e1b4
Ajay Susanth <ajaysusanth10@gmail.com>
2024-07-31 19:22:51 +0530

    add-edit-notes

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 
-const {authenticate} = require('./utilities')
+const {authenticate, authenticateToken} = require('./utilities')
 
 const app = express()
 
@@ -28,6 +28,7 @@ mongoose.connect(config.connectionString)
 })
 
 const User = require('./models/user.model')
+const Note = require('./models/note.model') 
 
 
 app.get('/',(req,res)=>{
@@ -108,6 +109,75 @@ app.post('/login',async (req,res)=>{
     }
 })
 
+// Add note
+
+app.post('/add-note',authenticateToken, async (req,res)=>{
+    const {title,content,tags} = req.body
+    const {user} = req.user
+
+    if(!title) {
+        return res.status(400).json({error:true,message:"Title is required"})
+    }
+    if(!content) {
+        return res.status(400).json({error:true,message:"Content is required"})
+    }
+
+    try{
+        const note = new Note({
+            title,
+            content,
+            tags: tags || [],
+            userId: user._id
+        })
+
+        await note.save()
+
+        res.json({error:false,message:"Note added successfully",note})
+    }
+    catch(err){
+        return res.status(500).json({error:true,message:"Internal Server error"})
+    }
+
+})
+
+// Edit note
+
+app.put('/edit-note/:id',authenticateToken, async (req,res)=>{
+    const noteId = req.params.id
+    const {title,content,tags,isPinned} = req.body
+    const {user} = req.user
+
+    if (!title && !content && !tags){
+        return res.status(400).json({error:true,message:'No changes provided'})
+    }
+
+    try{
+        const note = await Note.findOne({_id:noteId,userId:user._id})
+
+        if (!note) {
+            return res.status(404).json({error:true,message:'Note not found'})
+        }
+    
+        if (title) note.title = title
+        if (content) note.content = content
+        if (tags) note.tags = tags
+        if (isPinned) note.isPinned = isPinned
+
+        await note.save()
+
+        return res.json({
+            error:false,
+            note,
+            message:'Note edited successfully'
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            error:true,
+            message:"Internal Server error"
+        })
+    }
+})
 
 app.listen(8000)
 
