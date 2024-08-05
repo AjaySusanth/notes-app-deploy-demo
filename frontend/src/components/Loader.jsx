94ec5e425379044819b38048fd999c87aa839ee3
Ajay Susanth <89147144+AjaySusanth@users.noreply.github.com>
2024-08-06 01:18:36 +0530

    Create Loader.jsx

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,10 @@
+
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="loader border-t-4 border-blue-500 rounded-full w-16 h-16 animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loader;
